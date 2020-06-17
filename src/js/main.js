fbe8440b0722c93ab87314ca8a862cd123c3b71c
Nikolinjho <nikolinjho@gmail.com>
2020-06-17 15:24:00 +0300

    initial

---
+++ src/js/main.js
@@ -0,0 +1,16 @@
+import ElementQueries from 'css-element-queries/src/ElementQueries';
+
+
+
+window.addEventListener('load', () => {
+    // медиа выражения на основе ширины элементов, а не окна
+    ElementQueries.init(); 
+})
+
+
+$(document).ready(function(){
+    
+   
+
+  
+})
